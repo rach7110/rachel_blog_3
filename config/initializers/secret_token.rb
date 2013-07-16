e979ab6f7ea62b5d93c017586fe5af6f840c2d13
MakerSquare Student <students@themakersquare.com>
2013-07-16 16:40:41 -0500

    created new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RachelBlog3::Application.config.secret_token = 'e0fb634d398188b10f89fdc56c255f077b25bd836c5f2c49f08b0a5ac874b26f6a721049c27da3f0a6eee9b27fee2f8ac57de2fc7e84e52218805ce662f728ee'
