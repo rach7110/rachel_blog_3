7865df8401c140ea588e0e6ce1ee2f8197587e8d
Rachel <rachel.loziuk@gmail.com>
2013-07-17 13:42:17 -0500

    created a new route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 RachelBlog3::Application.routes.draw do
-  get '/posts' => 'posts#index'
+    root :to => 'posts#index'
+    get '/posts' => 'posts#new'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
